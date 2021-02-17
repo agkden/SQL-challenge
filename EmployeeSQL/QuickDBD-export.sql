74e97ca4d3247ec7d737c08ec74631e816104a0a
agkden <agkden@yahoo.com>
2021-02-17 00:40:22 -0700

    update file
    QuickDBD file updated to make table names case-insensitive

--- EmployeeSQL/QuickDBD-export.sql
+++ EmployeeSQL/QuickDBD-export.sql
@@ -3,7 +3,7 @@
 
 -- EmployeeSQL ERD
 
-CREATE TABLE "Employees" (
+CREATE TABLE "employees" (
     "emp_no" INT   NOT NULL,
     "emp_title_id" CHAR(5)   NOT NULL,
     "birth_date" DATE   NOT NULL,
@@ -11,74 +11,72 @@ CREATE TABLE "Employees" (
     "last_name" VARCHAR(50)   NOT NULL,
     "sex" CHAR(1)   NOT NULL,
     "hire_date" DATE   NOT NULL,
-    CONSTRAINT "pk_Employees" PRIMARY KEY (
+    CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
      )
 );
 
-CREATE TABLE "Titles" (
+CREATE TABLE "titles" (
     "title_id" CHAR(5)   NOT NULL,
     "title" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Titles" PRIMARY KEY (
+    CONSTRAINT "pk_titles" PRIMARY KEY (
         "title_id"
      ),
-    CONSTRAINT "uc_Titles_title" UNIQUE (
+    CONSTRAINT "uc_titles_title" UNIQUE (
         "title"
     )
 );
 
-CREATE TABLE "Salaries" (
+CREATE TABLE "salaries" (
     "emp_no" INT   NOT NULL,
     "salary" INT   NOT NULL,
-    CONSTRAINT "pk_Salaries" PRIMARY KEY (
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
         "emp_no"
      )
 );
 
-CREATE TABLE "Departments" (
+CREATE TABLE "departments" (
     "dept_no" CHAR(4)   NOT NULL,
     "dept_name" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Departments" PRIMARY KEY (
+    CONSTRAINT "pk_departments" PRIMARY KEY (
         "dept_no"
      ),
-    CONSTRAINT "uc_Departments_dept_name" UNIQUE (
+    CONSTRAINT "uc_departments_dept_name" UNIQUE (
         "dept_name"
     )
 );
 
-CREATE TABLE "Dept_emp" (
+CREATE TABLE "dept_emp" (
     "emp_no" INT   NOT NULL,
     "dept_no" CHAR(4)   NOT NULL,
-    CONSTRAINT "pk_Dept_emp" PRIMARY KEY (
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
         "emp_no","dept_no"
      )
 );
 
-CREATE TABLE "Dept_manager" (
+CREATE TABLE "dept_manager" (
     "dept_no" CHAR(4)   NOT NULL,
     "emp_no" INT   NOT NULL,
-    CONSTRAINT "pk_Dept_manager" PRIMARY KEY (
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
         "dept_no","emp_no"
      )
 );
 
-ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
-REFERENCES "Titles" ("title_id");
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
 
-ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Departments" ("dept_no");
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
 
-ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Departments" ("dept_no");
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
 
-ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
-
-
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
